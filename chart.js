ae23845c8fdacb9a06b0103143447eb3187bfb49
Rosie Tempest <rosietempest@gmail.com>
2021-01-17 14:28:18 -0800

    got the counties to show up on the svg

--- chart.js
+++ chart.js
@@ -1,9 +1,9 @@
 const urlEducation = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json'
-const urlCountry = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json'
+const urlCounties = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json'
 
 // width and height of svg
 const h = 600
-const w = 700
+const w = 1000
 
 // append svg
 const svg = d3.select('body')
@@ -11,14 +11,31 @@ const svg = d3.select('body')
     .attr('width', w)
     .attr('height', h)
 
+// define projection
+const proj = d3.geoAlbersUsa()
+
+// create paths
+const path = d3.geoPath()
+    .projection(proj)
 
 // get data
 d3.queue()
+    .defer(d3.json, urlCounties)
     .defer(d3.json, urlEducation)
-    .defer(d3.json, urlCountry)
     .await(makeMap);
 
 // make the map with the data
-function makeMap(error, education, country) {
-    console.log(country)
+function makeMap(error, us, education) {
+    // add counties layer
+    g = svg.append('g')
+        .selectAll('path')
+        .data(topojson.feature(us, us.objects.counties).features)
+        .enter()
+        .append('path')
+        .attr('class', 'county')
+        .attr('data-fips', d => d.id)
+        .attr('d', d3.geoPath())
+
+    // console.log(topojson.feature(us, us.objects.counties).features)
+
 }
